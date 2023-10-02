df72b6ffbb5b2943d683bbe7d71168377d566d09
KulichenkovD <135862041+KulichenkovD@users.noreply.github.com>
2023-10-02 22:35:05 +0300

    Create urls.py

---
+++ Django_project_News_Portal/News_Portal/News/urls.py
@@ -0,0 +1,17 @@
+from .views import *
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('author_list', AuthorList.as_view()),
+    path('news_list/', index, name='index'),
+    path('New/<str:slug>', detail, name='detail'),
+    path('search/<str:slug>', detail, name='detail'),
+    path('post/', PostChoices.as_view()),
+    path('post/<int:pk>/', PostDetail.as_view()),
+    #path('admin/', admin.site.urls),
+    path('accounts/',
+         #include('django.contrib.auth.urls')),
+         include('allauth.urls')),
+    path('accounts/', include('Accounts.urls')),
+]
